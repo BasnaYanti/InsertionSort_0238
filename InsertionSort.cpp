0b205af8c52acf4982eb4bcb3347a39ea23e47af
Basna Yanti <basna.yanti.ft24@mail.umy.ac.id>
2025-03-25 12:36:23 +0700

    Complete insertionsort procedure

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -6,5 +6,51 @@ int n;
 
 void input()
 {
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data pada Array : ";
+        cin >> n;
 
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksumal 20 Elemen.\n";
+        }
+    }
+    cout << endl;
+    cout << "======================" << endl;
+    cout << "Masukkan Element Array" << endl;
+    cout << "======================" << endl;
+
+    for (int i = 0; i< n; i++)
+    {
+        cout << "Data ke--" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
+
+void insertionSort()
+{
+
+        int temp;
+        int j, i;
+
+        for (i = 1; i <= n - 1; i++)
+        {
+
+            temp = arr[1];
+
+            j = i - 1;
+
+            while (j >= 0 && arr[j] > temp)
+            {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = temp;
+        }
 }
